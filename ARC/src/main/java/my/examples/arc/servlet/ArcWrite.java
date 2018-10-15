1e6b935d94e5f6c0d9c1edc89a47fd09758c40aa
KimJunHyeong <jhkim4685@gmail.com>
2018-10-15 20:43:56 +0900

    예외처리 및 sql 수정

--- ARC/src/main/java/my/examples/arc/servlet/ArcWrite.java
+++ ARC/src/main/java/my/examples/arc/servlet/ArcWrite.java
@@ -24,16 +24,24 @@ public class ArcWrite extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         // id, 상품이름, 투자 금액과 투자 기간을 검사한다.
-        HttpSession session = req.getSession();
-        String id = (String)session.getAttribute("id");
-        int gdsCd = Integer.parseInt(req.getParameter("gdsCd"));
-        double investPrice = Double.parseDouble(req.getParameter("investPrice"));
-        int investPeriod = Integer.parseInt(req.getParameter("investPeriod"));
+        HttpSession session = req.getSession(false);    // 이미 세션이 있다면 그 세션을 돌려주고, 세션이 없으면 null을 돌려준다.
+        String userID = (String) session.getAttribute("userID");
+        if(userID == null) {    // 로그인이 되어 있지 않으면 login 페이지로 이동
+            resp.sendRedirect("/login");
+            return;
+        }
+        try {
+            int goodsCode = Integer.parseInt(req.getParameter("goodsCode"));
+            double investmentPrice = Double.parseDouble(req.getParameter("investmentPrice"));
+            int investmentPeriod = Integer.parseInt(req.getParameter("investmentPeriod"));
 
-        // 상품, 투자 금액과 투자 기간을 DB에 저장한다.
-        ArcWriteDTO arcWriteDto = new ArcWriteDTO(id, gdsCd, investPeriod, investPrice);
-        ArcDAO arcDAO = new ArcDAO();
-        arcDAO.writeArc(arcWriteDto);
+            // 상품, 투자 금액과 투자 기간을 DB에 저장한다.
+            ArcWriteDTO arcWriteDto = new ArcWriteDTO(userID, goodsCode, investmentPeriod, investmentPrice);
+            ArcDAO arcDAO = new ArcDAO();
+            arcDAO.writeArc(arcWriteDto);
+        } catch (NumberFormatException ex) {
+            throw new RuntimeException("유효한 값 입력되지 않았습니다." + ex.toString());
+        }
 
         // /list로 리다이렉션
         resp.sendRedirect("/list");
--- ARC/webapp/WEB-INF/views/goodsAdd.jsp
+++ ARC/webapp/WEB-INF/views/goodsAdd.jsp
@@ -1,11 +1,3 @@
-<%--
-  Created by IntelliJ IDEA.
-
-  User: jycs
-  Date: 18. 10. 11
-  Time: 오후 7:29
-  To change this template use File | Settings | File Templates.
---%>
 <%@ page contentType="text/html;charset=UTF-8" language="java" pageEncoding="UTF-8" %>
 <html>
 <head>
@@ -38,7 +30,7 @@
                    aria-expanded="false">접속하기<span class="caret"></span></a>
                 <ul class="dropdown-menu">
                     <li><a href="/login">로그인</a></li>
-                    <li class="active"><a href="/join">회원가입</a></li>
+                    <li><a href="/join">회원가입</a></li>
                 </ul>
             </li>
         </ul>
